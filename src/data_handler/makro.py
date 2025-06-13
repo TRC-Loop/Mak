2ee9c06e20fdf8596e083e84ba4bdc40021afb70
TRC Loop <ak@stellar-code.com>
2025-06-13 19:29:19 +0200

    Fixed Makro

--- src/data_handler/makro.py
+++ src/data_handler/makro.py
@@ -4,16 +4,16 @@ import uuid
 import os
 
 class Makro:
-    def __init__(self, name: str, description: str, commands: list[str], json_path: str | None = None) -> None:
+    def __init__(self, name: str, description: str, commands: list[str], json_path: str | None = None, created: str | None = None, id: str | None = None) -> None:
         self.name = name
         self.description = description
         self.commands = commands
         self.json_path = json_path if json_path else "No path specified."
 
-        self.created = datetime.datetime.now()
-        self.id = uuid.uuid4()
+        self.created = datetime.datetime.now() if not created else created
+        self.id = uuid.uuid4() if not id else id
     
-    def save(self) -> Bool:
+    def save(self) -> bool:
         if not os.path.exists(self.json_path):
             with open(self.json_path, "w") as f:
                 f.write("[]")
